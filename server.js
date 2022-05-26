63bcd7ffb3abbb43e8004ba354fd85ad19f88579
maikalangi <maikalangi@gmail.com>
2022-05-26 13:29:31 -0700

    commits navigation partial

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ const userController = require('./controllers/users');
 app.use('/users', userController);
 const sessionsController = require('./controllers/sessions');
 app.use('/sessions', sessionsController);
+app.get('/', (req, res)=>{
+    res.render('index.ejs');
+});
 
 // Listener
 const PORT = process.env.PORT;
